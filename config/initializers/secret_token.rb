f3480ef9fc5fe461969b630265557121b763c541
rkiel <cygnus@pobox.com>
2014-04-05 11:48:24 -0400

    created todo application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pizza::Application.config.secret_key_base = '6938b4cde9a7f0ef946ba14f407bc3d2c22f049c529678d3dc4dd8679e1b43d35e36f75293931a75ac137054d40a90ec352f967559824c7da986a88c611529b9'
